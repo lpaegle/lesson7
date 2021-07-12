e87513017a76d80d22a468313379c593ef0123fb
lpaegle <livijapaegle@gmail.com>
2021-07-12 20:26:41 +0300

    Added switch/case

--- src/Lesson7.java
+++ src/Lesson7.java
@@ -2,6 +2,7 @@ public class Lesson7 {
     public static void main(String[] args) {
 
 //    //Alarm clock app
+//    //-------------------------
 //        String dayOfWeek;
 //
 //        dayOfWeek = "Tuesday";
@@ -18,28 +19,48 @@ public class Lesson7 {
 //            System.out.println("Not a week day");
 //        }
 
+//      //-------------------------
+//        // If you go to office
+//        // working day - high heels
+//        // Casual Fridays - you can wear flats
+//        // Holidays - you stay at home
+//        // Halloween - you can wear any costume
+//
+//        String day = "Working day";
+//
+//        if (day == "Working day"){
+//            System.out.println("You have to wear high heels.");
+//        }else if (day == "Casual friday"){
+//            System.out.println("You can wear flats.");
+//        }else if (day == "Holiday"){
+//            System.out.println("You can stay at home.");
+//        }else if (day == "Halloween"){
+//            System.out.println("You can wear a costume.");
+//        }else {
+//            System.out.println("This day doesn't exist.");
+//        }
 
-        // If you go to office
-        // working day - high heels
-        // Casual Fridays - you can wear flats
-        // Holidays - you stay at home
-        // Halloween - you can wear any costume
 
-        String day = "Working day";
+        //Implement the same office wear activity with Switch
 
-        if (day == "Working day"){
-            System.out.println("You have to wear high heels.");
-        }else if (day == "Casual friday"){
-            System.out.println("YOu can wear flats.");
-        }else if (day == "Holiday"){
-            System.out.println("You can stay at home.");
-        }else if (day == "Halloween"){
-            System.out.println("You can wear a costume.");
-        }else {
-            System.out.println("This day doesn't exist.");
+        String officeDay = "Working day";
+        switch (officeDay){
+            case "Working day":
+                System.out.println("You have to wear high heels.");
+                break;
+            case "Casual day":
+                System.out.println("You can wear flats.");
+                break;
+            case "Holiday":
+                System.out.println("You can stay at home.");
+                break;
+            case "Halloween":
+                System.out.println("You can wear a costume.");
+                break;
+            default:
+                System.out.println("This day doesn't exist.");
         }
 
-        
 
     }
 }
